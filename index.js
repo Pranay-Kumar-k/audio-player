42b40efd98569e3831eca9533c46faa4d8264172
pranay-tericsoft <pranay@tericsoft.com>
2021-09-06 02:00:08 +0530

    Added all the required files and basics

---
+++ index.js
@@ -0,0 +1,5 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext('2d');
+const audio = document.getElementById("audio");
+const button = document.getElementById("button");
+
