3be41fedb78b134dc9332db7e16d25086f24e66a
pranay-tericsoft <pranay@tericsoft.com>
2021-09-06 04:15:36 +0530

    Filling the bars with color according to the audio duration and jumping the audio by clicking on a bar functionalities added

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ const ctx = canvas.getContext('2d');
 const audio = document.getElementById("audio");
 const button = document.getElementById("button");
 const buttonClassList = button.querySelector("i.fas").classList
+const container = document.getElementById("container");
 
-let audioBars = [];
+let audioBars = [-30];
 var is_playing = false;
 // function used to draw the bars in the audio player visual
 const drawBars = () => {
 
-    for(var i=0; i<115; i++) {
-        audioBars.push(Math.floor(Math.random() * 250) + 1);
+    for(var i=0; i<160; i++) {
+        audioBars.push(Math.floor(Math.random() * 200) + 1);
     }
 
     let width = 8;
@@ -25,6 +26,7 @@ const drawBars = () => {
 }
 drawBars();
 
+//function to play/pause the audio
 button.addEventListener('click', () => {
     if(is_playing) {
         pauseAudio();
@@ -45,3 +47,32 @@ const pauseAudio = () => {
     audio.pause();
     is_playing = false;
 }
+
+// function to track the song duration
+const playAudioViaBars = (time) => {
+    let width = 8;
+    let X = 0;
+    for (let i = 0; i < audioBars.length; i++) {
+        let h = audioBars[i];
+        ctx.fillRect(X, 150 - h / 4, width, h+30);
+        X += width + 3;
+        X < time ? (ctx.fillStyle = "red") : (ctx.fillStyle = "gainsboro");
+    }
+}
+
+// Updating audio bars with color filled to track the duration of song/audio
+const updateAudioBars = (audioElement) => {
+    const {duration, currentTime} = audioElement.srcElement;
+    playAudioViaBars(currentTime * (1250/duration));
+}
+
+// Jump to a particular point of duration in audio by clicking on the bar
+const jumpAudio = (e) => {
+    const width = container.clientWidth;
+    const click = e.offsetX;
+    const duration = audio.duration;
+    audio.currentTime = (click / width) * duration;
+}
+
+audio.addEventListener("timeupdate", updateAudioBars);
+container.addEventListener("click", jumpAudio);
